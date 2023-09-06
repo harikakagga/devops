38b46627d6bef0d5602a11e5adb9978574f81854
root <root@ip-172-31-39-157.ap-south-1.compute.internal>
2023-09-06 22:25:19 +0000

    merge

---
+++ harika.java
@@ -0,0 +1,25 @@
+[200~Is – Displays information about files in the current directory.
+pwd – Displays the current working directory.
+mkdir – Creates a directory.
+cd – To navigate between different folders.
+rmdir – Removes empty directories from the directory lists.
+cp – Moves files from one directory to another.
+mv – Rename and Replace the files
+rm – Delete files 
+uname – Command to get basic information about the OS
+locate– Find a file in the database.
+touch – Create empty files
+ln – Create shortcuts to other files
+cat – Display file contents on terminal
+clear – Clear terminal 
+ps- Display the processes in terminal
+man – Access manual for all Linux commands
+grep- Search for a specific string in an output
+echo- Display active processes on the terminal
+wget – download files from the internet
+whoami- Create or update passwords for existing users
+sort- sort the file content
+cal- View Calendar in terminal
+whereis – View the exact location of any command types after this command
+df – Check the details of the file system
+wc – Check the lines, word count, and characters in a file using different options 
